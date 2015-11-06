d25f1166441cae7fc5a2a3eb7c6a37a1b097f97b
Harry Ganz <hganz@rsmas.miami.edu>
2015-11-06 10:14:35 -0500

    Adds invalid format exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def render_404
+    raise ActionController::RoutingError.new('Invalid Format')
+  end
+
+  rescue_from ActionController::UnknownFormat, :with => :render_404
+
 end
