1317aa03bfe97eb6850bd85b3d8ae7c46c2bbfe4
Andy Lindeman <andy.lindeman@noaa.gov>
2025-06-24 16:10:14 -0400

    Link public/data

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, "/var/www/apps/rvc_rails"
 append :linked_files, "config/master.key"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "storage", ".bundle", "node_modules"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "storage", ".bundle", "node_modules", "public/data"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
